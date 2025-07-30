29d9e68c8cf955fcc261810be5fd811876c3ef71
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-07-30 20:42:56 +0530

    setting up AppLayout

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,16 +1,18 @@
-function OrderItem({ item, isLoadingIngredients, ingredients }) {
-  const { quantity, name, totalPrice } = item;
+import { formatCurrency } from "../../utils/helpers";
 
-  return (
-    <li>
-      <div>
-        <p>
-          <span>{quantity}&times;</span> {name}
-        </p>
-        <p>{formatCurrency(totalPrice)}</p>
-      </div>
-    </li>
-  );
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+    const { quantity, name, totalPrice } = item;
+
+    return (
+        <li>
+            <div>
+                <p>
+                    <span>{quantity}&times;</span> {name}
+                </p>
+                <p>{formatCurrency(totalPrice)}</p>
+            </div>
+        </li>
+    );
 }
 
 export default OrderItem;
