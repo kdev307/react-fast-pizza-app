a4f4aa9dfbb13b193713f39301e11fc900ae8aed
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-08-15 13:25:00 +0530

    fetching menu data using react router 6.4 loaders

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -39,15 +39,7 @@ const order = {
 
 function Order() {
     // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-    const {
-        // id,
-        status,
-        priority,
-        priorityPrice,
-        orderPrice,
-        estimatedDelivery,
-        // cart,
-    } = order;
+    const { id, status, priority, priorityPrice, orderPrice, estimatedDelivery, cart } = order;
     const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
     return (
